006734ff4155411f4f2d034376f3d9fdc2e6c368
jocelyn <jpobleteg@gmail.com>
2016-10-29 21:43:24 -0300

    Add project files.

---
+++ BibliotecaEscuadron/Usuario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaEscuadron
+{
+    public class Usuario
+    {
+        private string username;
+        private string password;
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public Usuario()
+        {
+            this.Init();
+        }
+
+        private void Init()
+        {
+            username = string.Empty;
+            password = string.Empty;
+        }
+    }
+
+    
+}
