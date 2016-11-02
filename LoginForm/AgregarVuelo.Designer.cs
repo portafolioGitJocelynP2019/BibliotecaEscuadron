b99ef1182ed6dcfbf191eb273c0e83051d528889
Cristian Gacitua <cri.gacitua.flores@gmail.com>
2016-11-02 20:44:49 -0300

    Agregando nuevas vistas

--- LoginForm/AgregarVuelo.Designer.cs
+++ LoginForm/AgregarVuelo.Designer.cs
@@ -46,12 +46,18 @@
             this.label9 = new System.Windows.Forms.Label();
             this.btnIngresar = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblHora
             // 
             this.lblHora.AutoSize = true;
-            this.lblHora.Location = new System.Drawing.Point(809, 9);
+            this.lblHora.Location = new System.Drawing.Point(779, 451);
             this.lblHora.Name = "lblHora";
             this.lblHora.Size = new System.Drawing.Size(35, 13);
             this.lblHora.TabIndex = 0;
@@ -61,7 +67,7 @@
             // lblFecha
             // 
             this.lblFecha.AutoSize = true;
-            this.lblFecha.Location = new System.Drawing.Point(652, 9);
+            this.lblFecha.Location = new System.Drawing.Point(638, 451);
             this.lblFecha.Name = "lblFecha";
             this.lblFecha.Size = new System.Drawing.Size(35, 13);
             this.lblFecha.TabIndex = 1;
@@ -69,57 +75,57 @@
             // 
             // txtNroVuelo
             // 
-            this.txtNroVuelo.Location = new System.Drawing.Point(237, 107);
+            this.txtNroVuelo.Location = new System.Drawing.Point(300, 92);
             this.txtNroVuelo.Name = "txtNroVuelo";
-            this.txtNroVuelo.Size = new System.Drawing.Size(274, 20);
+            this.txtNroVuelo.Size = new System.Drawing.Size(355, 20);
             this.txtNroVuelo.TabIndex = 2;
             // 
             // txtOrigen
             // 
-            this.txtOrigen.Location = new System.Drawing.Point(237, 185);
+            this.txtOrigen.Location = new System.Drawing.Point(300, 236);
             this.txtOrigen.Name = "txtOrigen";
-            this.txtOrigen.Size = new System.Drawing.Size(274, 20);
+            this.txtOrigen.Size = new System.Drawing.Size(355, 20);
             this.txtOrigen.TabIndex = 4;
             // 
             // txtDestino
             // 
-            this.txtDestino.Location = new System.Drawing.Point(237, 211);
+            this.txtDestino.Location = new System.Drawing.Point(300, 278);
             this.txtDestino.Name = "txtDestino";
-            this.txtDestino.Size = new System.Drawing.Size(274, 20);
+            this.txtDestino.Size = new System.Drawing.Size(355, 20);
             this.txtDestino.TabIndex = 5;
             // 
             // txtMision
             // 
-            this.txtMision.Location = new System.Drawing.Point(237, 263);
+            this.txtMision.Location = new System.Drawing.Point(300, 361);
             this.txtMision.Name = "txtMision";
-            this.txtMision.Size = new System.Drawing.Size(274, 20);
+            this.txtMision.Size = new System.Drawing.Size(355, 20);
             this.txtMision.TabIndex = 6;
             // 
             // txtFecha
             // 
-            this.txtFecha.Location = new System.Drawing.Point(237, 237);
+            this.txtFecha.Location = new System.Drawing.Point(300, 320);
             this.txtFecha.Name = "txtFecha";
-            this.txtFecha.Size = new System.Drawing.Size(274, 20);
+            this.txtFecha.Size = new System.Drawing.Size(355, 20);
             this.txtFecha.TabIndex = 7;
             // 
             // txtTotalTv
             // 
-            this.txtTotalTv.Location = new System.Drawing.Point(237, 159);
+            this.txtTotalTv.Location = new System.Drawing.Point(300, 188);
             this.txtTotalTv.Name = "txtTotalTv";
-            this.txtTotalTv.Size = new System.Drawing.Size(274, 20);
+            this.txtTotalTv.Size = new System.Drawing.Size(355, 20);
             this.txtTotalTv.TabIndex = 8;
             // 
             // txtCondicion
             // 
-            this.txtCondicion.Location = new System.Drawing.Point(237, 133);
+            this.txtCondicion.Location = new System.Drawing.Point(300, 140);
             this.txtCondicion.Name = "txtCondicion";
-            this.txtCondicion.Size = new System.Drawing.Size(274, 20);
+            this.txtCondicion.Size = new System.Drawing.Size(355, 20);
             this.txtCondicion.TabIndex = 9;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(132, 110);
+            this.label1.Location = new System.Drawing.Point(179, 92);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(88, 13);
             this.label1.TabIndex = 10;
@@ -128,7 +134,7 @@
             // asdasd
             // 
             this.asdasd.AutoSize = true;
-            this.asdasd.Location = new System.Drawing.Point(182, 263);
+            this.asdasd.Location = new System.Drawing.Point(233, 368);
             this.asdasd.Name = "asdasd";
             this.asdasd.Size = new System.Drawing.Size(36, 13);
             this.asdasd.TabIndex = 13;
@@ -137,7 +143,7 @@
             // asdasdsd
             // 
             this.asdasdsd.AutoSize = true;
-            this.asdasdsd.Location = new System.Drawing.Point(184, 237);
+            this.asdasdsd.Location = new System.Drawing.Point(233, 327);
             this.asdasdsd.Name = "asdasdsd";
             this.asdasdsd.Size = new System.Drawing.Size(34, 13);
             this.asdasdsd.TabIndex = 14;
@@ -146,7 +152,7 @@
             // dddd
             // 
             this.dddd.AutoSize = true;
-            this.dddd.Location = new System.Drawing.Point(177, 214);
+            this.dddd.Location = new System.Drawing.Point(233, 285);
             this.dddd.Name = "dddd";
             this.dddd.Size = new System.Drawing.Size(43, 13);
             this.dddd.TabIndex = 15;
@@ -155,7 +161,7 @@
             // ioooo
             // 
             this.ioooo.AutoSize = true;
-            this.ioooo.Location = new System.Drawing.Point(177, 192);
+            this.ioooo.Location = new System.Drawing.Point(229, 243);
             this.ioooo.Name = "ioooo";
             this.ioooo.Size = new System.Drawing.Size(38, 13);
             this.ioooo.TabIndex = 16;
@@ -165,7 +171,7 @@
             // asd
             // 
             this.asd.AutoSize = true;
-            this.asd.Location = new System.Drawing.Point(175, 166);
+            this.asd.Location = new System.Drawing.Point(226, 195);
             this.asd.Name = "asd";
             this.asd.Size = new System.Drawing.Size(43, 13);
             this.asd.TabIndex = 17;
@@ -174,7 +180,7 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(164, 140);
+            this.label9.Location = new System.Drawing.Point(213, 143);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(54, 13);
             this.label9.TabIndex = 18;
@@ -182,9 +188,9 @@
             // 
             // btnIngresar
             // 
-            this.btnIngresar.Location = new System.Drawing.Point(358, 305);
+            this.btnIngresar.Location = new System.Drawing.Point(427, 417);
             this.btnIngresar.Name = "btnIngresar";
-            this.btnIngresar.Size = new System.Drawing.Size(153, 23);
+            this.btnIngresar.Size = new System.Drawing.Size(153, 43);
             this.btnIngresar.TabIndex = 19;
             this.btnIngresar.Text = "Ingresar";
             this.btnIngresar.UseVisualStyleBackColor = true;
@@ -192,19 +198,83 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(135, 305);
+            this.button2.Location = new System.Drawing.Point(236, 417);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(126, 23);
+            this.button2.Size = new System.Drawing.Size(126, 47);
             this.button2.TabIndex = 20;
             this.button2.Text = "Limpiar campos";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // ListadoVuelo
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(152, 489);
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(12, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Listado de vuelos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(12, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 13);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Registrar nuevo vuelo";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(12, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Eliminar vuelo";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(12, 205);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 13);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "Editar vuelo";
+            // 
+            // AgregarVuelo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(869, 489);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.btnIngresar);
             this.Controls.Add(this.label9);
@@ -223,9 +293,10 @@
             this.Controls.Add(this.txtNroVuelo);
             this.Controls.Add(this.lblFecha);
             this.Controls.Add(this.lblHora);
-            this.Name = "ListadoVuelo";
+            this.Name = "AgregarVuelo";
             this.Text = "ListadoAeronave";
             this.Load += new System.EventHandler(this.ListadoAeronave_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -251,5 +322,10 @@
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Button btnIngresar;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
